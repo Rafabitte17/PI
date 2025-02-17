660ff5d72a0f5e8a6522e41bcdb627cfcde6b7b6
Rafabitte17 <rafabitte1@gmail.com>
2025-02-17 07:55:33 -0300

    Feat #1

--- alunos/views.py
+++ alunos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from alunos.models import Estado
+from alunos.serializers import EstadoSerializer
 
-# Create your views here.
+
+class EstadoViewSet(ModelViewSet):
+    queryset = Estado.objects.all()
+    serializer_class = EstadoSerializer
--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'aluno',
+    'rest_framework',
+    'alunos',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'America/Sao-Paulo'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
